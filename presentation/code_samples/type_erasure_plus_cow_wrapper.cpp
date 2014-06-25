a0f4251f19b7e6795dcf8dc19ba96ba51a768e2b
Zach Laine <whatwasthataddress@gmail.com>
2014-06-24 22:20:59 -0500

    Add example code to optimization step 1.

--- presentation/code_samples/type_erasure_plus_cow_wrapper.cpp
+++ presentation/code_samples/type_erasure_plus_cow_wrapper.cpp
@@ -14,9 +14,15 @@ struct button
 
 int main ()
 {
+// sample(type_erasure_plus_ref_wrapper)
+button b;
+widget ref_widget(std::ref(b));   // Underlying object
+widget cref_widget(std::cref(b)); // not copied.
+// end-sample
+
 // sample(type_erasure_plus_cow_wrapper_usage)
 copy_on_write<widget> w_1(widget{button()});
-copy_on_write<widget> w_2 = w_1; // No copy.
+copy_on_write<widget> w_2 = w_1;    // No copy.
 widget & mutable_w_2 = w_2.write(); // Copy happens here.
 // end-sample
 
