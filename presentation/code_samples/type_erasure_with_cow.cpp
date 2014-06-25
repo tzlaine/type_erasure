4a247178057a8419083c5d97abd56f83269e8fd6
Zach Laine <whatwasthataddress@gmail.com>
2014-06-24 22:38:26 -0500

    Flesh out more optimization discussion.

---
+++ presentation/code_samples/type_erasure_with_cow.cpp
@@ -0,0 +1,21 @@
+#include "disjoint_apis.hpp"
+
+
+struct button
+{
+    void render () const {}
+    layout_geometry geometry () const { return layout_geometry(); }
+    void set_label () {}
+    void set_image () {}
+    // etc.
+};
+
+int main ()
+{
+// sample(type_erasure_plus_with_cow)
+widget w_1(widget{button()}); // Only 1 allocation here.
+widget w_2 = w_1;             // No copy.
+// end-sample
+
+    return 0;
+}
