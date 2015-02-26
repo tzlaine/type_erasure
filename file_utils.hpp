9fb2f5b1b1148eb7a54eac9c2295d6cef13ef9a2
T. Zachary Laine <whatwasthataddress@gmail.com>
2015-02-26 09:51:46 -0600

    Fix wonky Windows line endings in generated code.

--- file_utils.hpp
+++ file_utils.hpp
@@ -10,7 +10,7 @@ inline std::string file_slurp (const std::string & filename)
 {
     std::string retval;
 
-    std::ifstream ifs(filename, std::ifstream::in | std::ifstream::binary);
+    std::ifstream ifs(filename);
     ifs.seekg(0, std::ifstream::end);
     if (0 < ifs.tellg())
         retval.resize(ifs.tellg());
@@ -34,11 +34,18 @@ inline std::vector<std::string> line_break (const std::string & file)
 
     std::string::size_type prev_pos = 0;
     while (true) {
-        std::string::size_type pos = file.find('\n', prev_pos);
-        if (pos == std::string::npos)
-            break;
+        std::string::size_type pos_1 = file.find("\r\n", prev_pos);
+        std::string::size_type pos_2 = file.find_first_of("\r\n", prev_pos);
+        std::string::size_type pos = pos_2;
+        std::string::size_type match_size = 1;
+        if (pos_1 != std::string::npos) {
+            pos = pos_1;
+            match_size = 2;
+        } else if (pos_2 == std::string::npos) {
+	        break;
+        }
         retval.push_back(file.substr(prev_pos, pos - prev_pos));
-        prev_pos = pos + 1;
+        prev_pos = pos + match_size;
     }
 
     retval.push_back(file.substr(prev_pos));
